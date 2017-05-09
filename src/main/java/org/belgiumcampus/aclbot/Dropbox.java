a82481522cf501b85e15ac61845d6d983ffa4d66
Richard Corbishley <riccorbypro@gmail.com>
2017-05-09 16:56:18 +0200

    Added Dropbox

---
+++ src/main/java/org/belgiumcampus/aclbot/Dropbox.java
@@ -0,0 +1,16 @@
+package org.belgiumcampus.aclbot;
+
+public class Dropbox implements Runnable {
+    
+    private String filePath;
+    
+    public Dropbox(String filePath){
+        this.filePath = filePath;
+    }
+
+    @Override
+    public void run() {
+        
+    }
+    
+}
